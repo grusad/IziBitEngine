e565e3301372e94a546fcf106b3ddbce5149ad79
Alexander Gillberg <gillberg.alexander@gmail.com>
2019-07-23 09:16:01 +0200

    another one

--- IziBitEngine/src/engine/Renderer.java
+++ IziBitEngine/src/engine/Renderer.java
@@ -14,10 +14,13 @@ public class Renderer {
     public void fillRect(int x, int y, int width, int height, IBColor color) {
         for(int x0 = x; x0 < x + width; x0++) {
             for(int y0 = y; y0 < y + height; y0++) {
-
                 drawPixel(x0, y0, color);
             }
         }
+    }
+
+    public void fillRect(Vector2 vector, int width, int height, IBColor color){
+        fillRect(vector.x, vector.y, width, height, color);
     }
 
     public void drawRect(int x, int y, int width, int height, IBColor color) {
@@ -30,6 +33,10 @@ public class Renderer {
             drawPixel(x, y0, color);
             drawPixel(x + width - 1, y0, color);
         }
+    }
+
+    public void drawRect(Vector2 vector, int width, int height, IBColor color){
+        drawRect(vector.x, vector.y, width, height, color);
     }
 
     public void drawCircle(int x, int y, int radius, IBColor color) {
@@ -65,12 +72,20 @@ public class Renderer {
         }
     }
 
+    public void drawCircle(Vector2 vector, int radius, IBColor color){
+        drawCircle(vector.x, vector.y, radius, color);
+    }
+
     public void drawTri(int x0, int y0, int x1, int y1, int x2, int y2, IBColor color) {
 
         drawLine(x0, y0, x1, y1, color);
         drawLine(x1, y1, x2, y2, color);
         drawLine(x2, y2, x0, y0, color);
 
+    }
+
+    public void drawTri(Vector2 vector0, Vector2 vector1, Vector2 vector2, IBColor color){
+        drawTri(vector0.x, vector0.y, vector1.x, vector1.y, vector2.x, vector2.y, color);
     }
 
     public void drawLine(int x0, int y0, int x1, int y1, IBColor color) {
@@ -84,6 +99,10 @@ public class Renderer {
         }
     }
 
+    public void drawLine(Vector2 vector0, Vector2 vector1, IBColor color){
+        drawLine(vector0.x, vector0.y, vector1.x, vector1.y, color);
+    }
+
     public void drawImage(int x, int y, IBImage image) {
         for(int x0 = x; x0 < x + image.getWidth(); x0++) {
             for(int y0 = y; y0 < y + image.getHeight(); y0++) {
@@ -92,10 +111,18 @@ public class Renderer {
         }
     }
 
+    public void drawImage(Vector2 vector, IBImage image){
+        drawImage(vector.x, vector.y, image);
+    }
+
     public void drawPixel(int x, int y, IBColor color) {
         if(!isOutOfBounds(x, y)) {
             pixels[x + y * display.getWidth()] = color.getHex();
         }
+    }
+
+    public void drawPixel(Vector2 vector, IBColor color){
+        drawPixel(vector.x, vector.y, color);
     }
 
     public void drawPixel(int x, int y, int pixel) {
@@ -116,8 +143,16 @@ public class Renderer {
         }
     }
 
+    public void drawString(Vector2 vector, String text, Font font){
+        drawString(vector.x, vector.y, text, font);
+    }
+
     public void drawString(int x, int y, String text){
         drawString(x, y, text, currentFont);
+    }
+
+    public void drawString(Vector2 vector, String text){
+        drawString(vector.x, vector.y, text);
     }
 
     public void setFont(Font font){
--- Sandbox/src/TestProgram.java
+++ Sandbox/src/TestProgram.java
@@ -6,11 +6,8 @@ import java.awt.*;
 
 public class TestProgram extends IziBitApplication {
 
-    private int x = 10;
-
-
     public TestProgram(){
-        super(new IziBitConfiguration(1280, 720, 1, "Test application"));
+        super(new IziBitConfiguration(1280, 720, 2, "Test application"));
     }
     @Override
     public void initialize() {
@@ -19,12 +16,12 @@ public class TestProgram extends IziBitApplication {
 
     @Override
     public void update() {
+
     }
 
     @Override
     public void render(Renderer renderer) {
-        renderer.drawString(0, 0, "FRAMES-" + getFrames());
-        renderer.drawString(0, 10, "UPDATES-" + getUpdates());
+
     }
 
     public static void main(String[] args){
